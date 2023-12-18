2fc4c8367a5c2be55d30f36adae82007fc1ca11e
Jeong Jun Ahn <tyyyu1268@gmail.com>
2023-12-18 23:13:15 +0100

    Solved Foundations

--- domjudge/03_5.c
+++ domjudge/03_5.c
@@ -1,121 +1,54 @@
-/*
 #include <stdio.h>
 #include <stdlib.h>
 
-void readInput(int *n, int *m, int *w, int *h, int ***land) {
-    scanf("%d %d %d %d", n, m, w, h);
-
-    *land = (int **)malloc(*m * sizeof(int *));
-
-    for (int i = 0; i < *m; i++) {
-        (*land)[i] = (int *)malloc(*n * sizeof(int));
-        for (int j = 0; j < *n; j++) {
-            scanf("%d", &(*land)[i][j]);
-        }
-    }
-}
-
-int minimumCost(int n, int m, int w, int h, int **land) {
-    int minCost = 0;
-
-    for (int i = 0; i <= n - w; i++) {
-        for (int j = 0; j <= m - h; j++) {
-            int cost = 0;
-
-            for (int k = i; k < i + w; k++) {
-                for (int l = j; l < j + h; l++) {
-                    cost += land[l][k];
-                }
-            }
-
-            if (minCost == 0 || cost < minCost) {
-                minCost = cost;
-            }
-        }
-    }
-
-    return minCost;
-}
-
 int main() {
     int n, m, w, h;
-    int **land;
-    readInput(&n, &m, &w, &h, &land);
+    scanf("%d %d %d %d", &n, &m, &w, &h);
 
-    int minCost = minimumCost(n, m, w, h, land);
+    int **land = (int **)malloc(m * sizeof(int *));
+
+	for (int i = 0; i < m; i++) {
+        land[i] = (int *)malloc(n * sizeof(int));
+    }
+
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            scanf("%d", &land[i][j]);
+        }
+    }
+
+    int minCost = 1e9;
+
+    for (int i = 0; i <= m - h; i++) {
+        int sum = 0;
+
+        for (int k = i; k < i + h; k++) {
+            for (int l = 0; l < w; l++) {
+                sum += land[k][l];
+            }
+        }
+
+        if (sum < minCost) {
+            minCost = sum;
+        }
+
+        for (int j = 1; j <= n - w; j++) {
+            for (int k = i; k < i + h; k++) {
+                sum = sum - land[k][j - 1] + land[k][j + w - 1];
+            }
+
+            if (sum < minCost) {
+                minCost = sum;
+            }
+        }
+    }
+
     printf("%d\n", minCost);
 
     for (int i = 0; i < m; i++) {
         free(land[i]);
     }
+
     free(land);
-
     return 0;
-} 
-*/
-
-#include <stdio.h>
-#include <stdlib.h>
-
-void readInput(int *n, int *m, int *w, int *h, int ***land) {
-    scanf("%d %d %d %d", n, m, w, h);
-
-    *land = (int **)malloc(*m * sizeof(int *));
-
-    for (int i = 0; i < *m; i++) {
-        (*land)[i] = (int *)malloc(*n * sizeof(int));
-        for (int j = 0; j < *n; j++) {
-            scanf("%d", &(*land)[i][j]);
-        }
-    }
 }
-
-int minCost(int n, int m, int w, int h, int **land) {
-    int **totalSum = (int **)malloc((m + 1) * sizeof(int *));
-
-    for (int i = 0; i <= m; i++) {
-        totalSum[i] = (int *)calloc((n + 1), sizeof(int));
-    }
-
-    for (int i = 1; i <= m; i++) {
-        for (int j = 1; j <= n; j++) {
-            totalSum[i][j] = totalSum[i - 1][j] + totalSum[i][j - 1] - totalSum[i - 1][j - 1] + land[i - 1][j - 1];
-        }
-    }
-
-    int minCost = 0;
-
-    for (int i = w; i <= n; i++) {
-        for (int j = h; j <= m; j++) {
-            int cost = totalSum[j][i] - totalSum[j - h][i] - totalSum[j][i - w] + totalSum[j - h][i - w];
-
-			if (minCost == 0 || cost < minCost) {
-                minCost = cost;
-            }
-        }
-    }
-
-    for (int i = 0; i <= m; i++) {
-        free(totalSum[i]);
-    }
-
-    free(totalSum);
-    return minCost;
-}
-
-int main() {
-	int n, m, w, h;
-	int **land;
-	readInput(&n, &m, &w, &h, &land);
-
-	int result = minCost(n, m, w, h, land);
-	printf("%d\n", result);
-
-	for (int i = 0; i <= m; i++) {
-		free(land[i]);
-	}
-
-	free(land);
-	return 0;
-}
-
